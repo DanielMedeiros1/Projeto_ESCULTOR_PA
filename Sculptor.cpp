ead38e550dfd7b283c1636ba1dd3728c862497d8
DanielMedeiros1 <daniel.medeiros.067@ufrn.edu.br>
2025-06-01 02:40:09 -0300

    aaaaaaa

--- Sculptor.cpp
+++ Sculptor.cpp
@@ -1,6 +1,8 @@
 #include "Sculptor.h"
 #include <fstream>
 #include <iostream>
+#include <iomanip>
+
 
 Sculptor::Sculptor(int nx_, int ny_, int nz_){
 
@@ -42,13 +44,13 @@ void Sculptor::setColor(float r, float g, float b, float a){
 
 void Sculptor::putVoxel(int x, int y, int z){
     
-    if (x >= 0 && x < nx && y >= 0 && y < ny && z >= 0 && z < nz) 
+    if (x >= 0 && x < nx && y >= 0 && y < ny && z >= 0 && z < nz){ 
         v[x][y][z].show = true;
         v[x][y][z].r = r;
         v[x][y][z].g = g;
         v[x][y][z].b = b;
         v[x][y][z].a = a;
-
+    }
 }
 
 void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1){
@@ -64,8 +66,8 @@ void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1){
 
 void Sculptor::writeOFF(const char *filename){
 
-    std::ofstream fout(filename);
-        if (!fout.is_open()){
+    std::ofstream outfile(filename);
+        if (!outfile.is_open()){
             std::cerr << "Erro \n";
             return;
         }
@@ -83,9 +85,9 @@ void Sculptor::writeOFF(const char *filename){
     }
 
    
-    fout << "OFF\n";
+    outfile << "OFF\n";
 
-    fout << totalVoxels * 8 << " " << totalVoxels * 6 << " 0\n";
+    outfile << totalVoxels * 8 << " " << totalVoxels * 6 << " 0\n";
 
     for (int i = 0; i < nx; i++) {
         for (int j = 0; j < ny; j++) {
@@ -93,65 +95,70 @@ void Sculptor::writeOFF(const char *filename){
                 if (v[i][j][k].show) {
                    
                     float x = i - 0.5, y = j - 0.5, z = k - 0.5; // Centralizado
-                    fout << x << " " << y << " " << z << "\n"; // Vértice 0
-                    fout << x << " " << y + 1 << " " << z << "\n"; // Vértice 1
-                    fout << x + 1 << " " << y + 1 << " " << z << "\n"; // Vértice 2
-                    fout << x + 1 << " " << y << " " << z << "\n"; // Vértice 3
-                    fout << x << " " << y << " " << z + 1 << "\n"; // Vértice 4
-                    fout << x << " " << y + 1 << " " << z + 1 << "\n"; // Vértice 5
-                    fout << x + 1 << " " << y + 1 << " " << z + 1 << "\n"; // Vértice 6
-                    fout << x + 1 << " " << y << " " << z + 1 << "\n"; // Vértice 7
+                    outfile << x << " " << y << " " << z << "\n"; // Vértice 0
+                    outfile << x << " " << y + 1 << " " << z << "\n"; // Vértice 1
+                    outfile << x + 1 << " " << y + 1 << " " << z << "\n"; // Vértice 2
+                    outfile << x + 1 << " " << y << " " << z << "\n"; // Vértice 3
+                    outfile << x << " " << y << " " << z + 1 << "\n"; // Vértice 4
+                    outfile << x << " " << y + 1 << " " << z + 1 << "\n"; // Vértice 5
+                    outfile << x + 1 << " " << y + 1 << " " << z + 1 << "\n"; // Vértice 6
+                    outfile << x + 1 << " " << y << " " << z + 1 << "\n"; // Vértice 7
                 }
             }
         }
     }
 
-    
-    int vertexOffset = 0; 
+    int offset = 0;
     for (int i = 0; i < nx; i++) {
-        for (int j = 0; j < ny; j++) {
-            for (int k = 0; k < nz; k++) {
-                if (v[i][j][k].show) {
-                    
-                    float r = v[i][j][k].r;
-                    float g = v[i][j][k].g;
-                    float b = v[i][j][k].b;
-                    float a = v[i][j][k].a;
+      for (int j = 0; j < ny; j++) {
+        for (int k = 0; k < nz; k++) {
+          if (v[i][j][k].show){
+          
+            outfile << std::fixed
+                    << std::setprecision(2);
 
-                    
-                    // Face frontal (Z+)
-                    fout << "4 " << vertexOffset + 4 << " " << vertexOffset + 5 << " "
-                         << vertexOffset + 6 << " " << vertexOffset + 7 << " "
-                         << r << " " << g << " " << b << " " << a << "\n";
-                    // Face traseira (Z-)
-                    fout << "4 " << vertexOffset + 0 << " " << vertexOffset + 1 << " "
-                         << vertexOffset + 2 << " " << vertexOffset + 3 << " "
-                         << r << " " << g << " " << b << " " << a << "\n";
-                    // Face inferior (Y-)
-                    fout << "4 " << vertexOffset + 0 << " " << vertexOffset + 3 << " "
-                         << vertexOffset + 7 << " " << vertexOffset + 4 << " "
-                         << r << " " << g << " " << b << " " << a << "\n";
-                    // Face superior (Y+)
-                    fout << "4 " << vertexOffset + 1 << " " << vertexOffset + 2 << " "
-                         << vertexOffset + 6 << " " << vertexOffset + 5 << " "
-                         << r << " " << g << " " << b << " " << a << "\n";
-                    // Face esquerda (X-)
-                    fout << "4 " << vertexOffset + 0 << " " << vertexOffset + 1 << " "
-                         << vertexOffset + 5 << " " << vertexOffset + 4 << " "
-                         << r << " " << g << " " << b << " " << a << "\n";
-                    // Face direita (X+)
-                    fout << "4 " << vertexOffset + 3 << " " << vertexOffset + 2 << " "
-                         << vertexOffset + 6 << " " << vertexOffset + 7 << " "
-                         << r << " " << g << " " << b << " " << a << "\n";
+            outfile << "4 " << 4 + offset << " " << 5 + offset << " "
+                    << 6 + offset << " " << 7 + offset << " " << v[i][j][k].r
+                    << " " << v[i][j][k].g << " " << v[i][j][k].b << " "
+                    << v[i][j][k].a << std::endl;
 
-                    vertexOffset += 8; 
-                }
-            }
+            outfile << "4 " << 0 + offset << " " << 1 + offset << " "
+                    << 2 + offset << " " << 3 + offset << " " << v[i][j][k].r
+                    << " " << v[i][j][k].g << " " << v[i][j][k].b << " "
+                    << v[i][j][k].a << std::endl;
+
+            outfile << "4 " << 0 + offset << " " << 3 + offset << " "
+                    << 7 + offset << " " << 4 + offset << " " << v[i][j][k].r
+                    << " " << v[i][j][k].g << " " << v[i][j][k].b << " "
+                    << v[i][j][k].a << std::endl;
+                  
+            outfile << "4 " << 1 + offset << " " << 2 + offset << " "
+                    << 6 + offset << " " << 5 + offset << " " << v[i][j][k].r
+                    << " " << v[i][j][k].g << " " << v[i][j][k].b << " "
+                    << v[i][j][k].a << std::endl;
+
+            outfile << "4 " << 0 + offset << " " << 1 + offset << " "
+                    << 5 + offset << " " << 4 + offset << " " << v[i][j][k].r
+                    << " " << v[i][j][k].g << " " << v[i][j][k].b << " "
+                    << v[i][j][k].a << std::endl;
+
+            outfile << "4 " << 3 + offset << " " << 2 + offset << " "
+                    << 6 + offset << " " << 7 + offset << " " << v[i][j][k].r
+                    << " " << v[i][j][k].g << " " << v[i][j][k].b << " "
+                    << v[i][j][k].a << std::endl;
+          
+            offset += 8;
+
         }
+      }
     }
+  }
 
-    fout.close();
-    std::cout << "Arquivo " << filename << " criado!\n";
+  
+  outfile.close();
+
+  std ::cout << "Arquivo " << filename << " criado" << std::endl;
 }
+
 
 
--- main.cpp
+++ main.cpp
@@ -1,26 +1,13 @@
 #include "Sculptor.h"
-#include <cstdlib> 
 
-int main() {
+int main(){
     
-    Sculptor diamante(16, 16, 16); 
+    Sculptor cubo(16, 16, 16); 
 
-    diamante.setColor(0.5, 0.5, 0.5, 1.0);
-    diamante.putBox(0, 15, 0, 15, 0, 15); 
+    cubo.setColor(1.0, 0.0, 0.0, 1.0);
+    cubo.putBox(0, 15, 0, 15, 0, 15); 
 
+    cubo.writeOFF("cubo.off");
     
-    diamante.setColor(0.25, 0.65, 1.0, 1.0); 
-    for (int i = 0; i < 10; i++) { 
-        int x = rand() % 14 + 1; 
-        int y = rand() % 14 + 1;
-        int z = rand() % 14 + 1;
-        
-        diamante.putVoxel(x, y, z);
-        diamante.putVoxel(x+1, y, z);
-        diamante.putVoxel(x, y+1, z);
-        diamante.putVoxel(x, y, z+1);
-    }
-
-    diamante.writeOFF("diamante.off");
     return 0;
 }
